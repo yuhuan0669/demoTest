feb284d9f24cfa95c7ebbe9415d55763ef9f3f94
yuhuan0669 <31118604+yuhuan0669@users.noreply.github.com>
2019-02-09 20:09:12 +0800

    Update TestController.java
    test 1

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -15,8 +15,8 @@ public class TestController {
 
     @RequestMapping("test")
     public String test() {
-        System.out.println("jenkins修改测试。。。");
-        return "jenkins修改测试";
+        System.out.println("ben jenkins修改测试。。。");
+        return "ben jenkins修改测试";
     }
 
 }
